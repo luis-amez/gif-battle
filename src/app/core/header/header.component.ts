32892625d8e90866d5b60cc96b53e7c48e778d6f
Luis Amez <luis.amez.vitoria@gmail.com>
2018-01-04 18:12:47 +0100

    Edit header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -3,9 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-    <p>
-      header works!
-    </p>
+    <nav class="navbar is-info">
+      <div class="container">
+        <!-- Logo -->
+        <div class="navbar-brand">
+          <a class="navbar-item">🔥 Gif Battle</a>
+        </div>
+        <!-- Menu -->
+        <div class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item">⚡ Battle</a>
+            <a class="navbar-item">🔨 Create</a>
+            <a class="navbar-item">🌟 Leaderboard</a>
+          </div>
+        </div>
+      </div>
+    </nav>
   `,
   styles: []
 })
