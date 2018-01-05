36f284450c15eb7955a5bb8bf8a35097cc140139
Luis Amez <luis.amez.vitoria@gmail.com>
2018-01-05 10:04:22 +0100

    Edit create component: displays a gif and caption input

--- src/app/pages/create/create.component.ts
+++ src/app/pages/create/create.component.ts
@@ -1,19 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { GifService } from '@app/core/services/gif.service';
 
 @Component({
   selector: 'app-create',
   template: `
-    <p>
-      create works!
-    </p>
+    <h1 class="title has-text-centered">Create!</h1>
+    <div class="box">
+      <div class="gif-container" *ngIf="randomGif">
+        <!-- image -->
+        <img [src]="randomGif.url">
+        <!-- caption -->
+        <div class="caption">{{ caption }}</div>  
+      </div>
+      <!-- caption input -->
+      <div class="field">
+        <input type="text" class="input" [(ngModel)]="caption">
+      </div>
+      <!-- create button -->
+      <a class="button is-warning" (click)="saveGif()">Create</a>
+    </div>
   `,
-  styles: []
+  styles: [
+    `
+      .button {
+        display: block;
+        width: 100%;
+      }
+    `
+  ]
 })
 export class CreateComponent implements OnInit {
+  randomGif;
+  caption = "";
 
-  constructor() { }
+  constructor(private gifService: GifService) { }
 
   ngOnInit() {
+    this.gifService.getRandom()
+      .subscribe(gif => {
+        this.randomGif = gif;
+      })
   }
 
 }
