086b1cc7b9ca80d416d8dc413e23f382828d7d9e
Luis Amez <luis.amez.vitoria@gmail.com>
2018-01-14 21:36:08 +0100

    Edit leaderboard component: display top ten gifs

--- src/app/pages/leaderboard/leaderboard.component.ts
+++ src/app/pages/leaderboard/leaderboard.component.ts
@@ -1,19 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { GifService } from '@app/core/services/gif.service';
+import { Gif } from '@app/models/gif.model';
 
 @Component({
   selector: 'app-leaderboard',
   template: `
-    <p>
-      leaderboard works!
-    </p>
+    <h1 class="title has-text-centered">Leaderboard</h1>
+
+    <table class="table is-bordered is-hover is-striped" *ngIf="leaderboardGifs">
+      <tbody>
+        <tr *ngFor="let gif of leaderboardGifs; let i = index">
+          <td>{{ i + 1 }}</td>
+          <td>
+            <app-gif
+              [url]="gif.url"
+              [caption]="gif.caption"
+            >
+            </app-gif>
+          </td>
+          <td>{{ gif.votes }}</td>
+        </tr>
+      </tbody>
+    </table>
   `,
   styles: []
 })
 export class LeaderboardComponent implements OnInit {
+  leaderboardGifs: Gif[];
 
-  constructor() { }
+  constructor(private gifService: GifService) { }
 
   ngOnInit() {
+    this.getLeaderboard();
   }
 
+  getLeaderboard() {
+    this.gifService.getLeaderboard()
+      .subscribe(gifs => {
+        this.leaderboardGifs = gifs;
+      });
+  }
 }
